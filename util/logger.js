e87cd457ebda91ba3b69ab415884993f6cb865f7
cxx <469350450@qq.com>
2019-11-11 16:05:45 +0800

    update

--- util/logger.js
+++ util/logger.js
@@ -1,7 +1,6 @@
 'use strict';
 var log4js = require('log4js');
 var logDir = 'logs'+100 ;
-var curTime = new Date().toLocaleString();
 
 log4js.configure({
     appenders: {
@@ -9,17 +8,20 @@ log4js.configure({
             type: "console"
         },
         rule_file: {
-            type: "file",
-            filename: __dirname + '/../'+logDir+'/debug/log-'+curTime+'.log',
-            maxLogSize: 1048576,
-			backups: 5,
+            type: "dateFile",
+            filename: __dirname + '/../'+logDir+'/debug/log-',
+            encoding: "utf-8",
+            maxLogSize: 1048576 * 10,
+            numBackups: 10,
+            pattern: "yyyy-MM-dd.log",
+            alwaysIncludePattern: true
         },
         rule_mark: {
             type: "dateFile",
             filename: __dirname + '/../'+logDir+'/debug/a_mark-',
             encoding: "utf-8",
-            maxLogSize: 1048576,
-            //numBackups: 3,
+            // maxLogSize: 1048576,
+            // numBackups: 3,
             pattern: "yyyy-MM-dd.log",
             alwaysIncludePattern: true
         }
